f2ed88e4cbd6adaa6c61edf67456af2e8f2fbde6
Cristi Miloiu <miloiuc4@gmail.com>
2024-08-26 11:01:15 +0300

    init

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
-import genezioLocalSDKReload from "@genezio/vite-plugin-genezio";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte(), genezioLocalSDKReload()],
+  plugins: [svelte()],
 })
